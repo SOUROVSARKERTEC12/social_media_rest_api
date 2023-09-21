d7926bdee8bf6b1fd542f29ab48806870de2cbd8
Admin_laravel <sourovsarker005@gmail.com>
2023-09-22 00:08:57 +0600

    update post

--- routes/posts.js
+++ routes/posts.js
@@ -3,6 +3,7 @@ const Post = require("../models/Post");
 const User = require("../models/User");
 
 //create a post
+
 router.post("/", async (req, res) => {
     const newPost = new Post(req.body);
     try {
@@ -13,5 +14,26 @@ router.post("/", async (req, res) => {
     }
 });
 
+//update a post
+
+router.put("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.userId === req.body.userId) {
+            await post.updateOne({ $set: req.body });
+            res.status(200).json("the post has been updated");
+        } else {
+            res.status(403).json("you can update only your post");
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//delete a post
+//like and dislike a post
+//get a post
+//get timeline a post
+//
 
 module.exports = router;
